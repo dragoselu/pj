13591d54e41c37ce5e0ecf323d1f1347f0be1808
dragos <root@fttb4.localdomain>
2012-02-02 15:16:51 +0200

    shopProduct create classes

--- index.php
+++ index.php
@@ -34,5 +34,105 @@ class ShopProduct
     {
         return $this->discount;
     }
+
+    public function getTtitle()
+    {
+        return $this->title;
+    }
+
+    public function getPrice()
+    {
+        return ($this-price - $this->discount);
+    }
+
+    public function getProducer()
+    {
+        return "{$this->producerFirstName}".
+            " {$this->producerMainName}";
+    }
+
+    public function getSummaryLine()
+    {
+        $base  = "{$this->title} ({$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName})";
+
+        return $base;
+    }
 }
+
+class CdProduct extends ShopProduct
+{
+    private $playLength = 0;
+
+    public function __construct($title, $firstName, $mainName, $price, $playLength)
+    {
+        parent::__construct($title, $firstName, $mainName, $price);
+        $this->playLength = $playLength;
+    }
+
+    public function getPlayLength()
+    {
+        return $this->playLength;
+    }
+
+    public function getSummaryLine()
+    {
+        $base  = parent::getSummaryLine();
+        $base .= ": playing time - {$this->playLength}";
+
+        return $base;
+    }
+}
+
+class BookProduct extends ShopProduct
+{
+    private $numPages = 0;
+
+    public function __construct($title, $firstName, $mainName, $price, $numPages)
+    {
+        parent::__construct($title, $firstName, $mainName, $price);
+        $this->numPages = $numPages;
+    }
+
+    public function getNumberOfPages()
+    {
+        return $this->numPages;
+    }
+
+    public function getSummaryLine()
+    {
+        $base  = parent::getSummaryLine();
+        $base .= ": page count - {$this->numPages}";
+
+        return $base;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
+class ShopProductWriter
+{
+    public $products = array();
+
+    public function addProduct(ShopProduct $shopProduct)
+    {
+        $this->products[] = $shopProduct;
+    }
+
+    public function write()
+    {
+        $str = "";
+        foreach($this->products as $shopProduct)
+        {
+            $str .= "{$shopProduct->title}: ";
+            $str .= $shopProduct->getProducer();
+            $str .= " ({$shopProduct->getPrice()})\n";
+        }
+        echo $str;
+    }
+}
+
 ?>
