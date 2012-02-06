48f4fd12541ab2c5e52a4eb67878a9b0a99d3626
Dragos Digulescu <dragoselu@gmail.com>
2012-02-06 23:05:24 +0200

    writer abstract

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ $dsn = "sqlite:products.sqlite";
 $pdo = new PDO($dsn, null, null);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $obj = ShopProduct::getInstance(2, $pdo);
-var_dump($obj);
+
+$writer = new XmlProductWriter($obj);
+$writer->write();
 ?>
