706c1dfb6d45c7bbbd0c8eb393d5aab3db9dd802
dragos <root@fttb4.localdomain>
2012-02-08 11:03:12 +0200

    db - added

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 include "shop.php";
-$dsn = "sqlite:products.sqlite";
+$dsn = "sqlite:products.db";
 
 $pdo = new PDO($dsn, null, null);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$obj = ShopProduct::getInstance(2, $pdo);
+$obj = ShopProduct::getInstance(1, $pdo);
 
 $writer = new TextProductWriter();
 $writer->addProduct($obj);
